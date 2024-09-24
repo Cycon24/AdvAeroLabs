87f09681beb8c6b80292c2454bbe819472b442d1
Cycon24 <cycongamingnight@gmail.com>
2024-09-24 17:51:14 -0400

    Added more things

--- Lab_02/main.py
+++ Lab_02/main.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # =============================================================================
 # Retrieve and organize data
 # =============================================================================
@@ -17,24 +18,30 @@ df_2 = pd.read_csv('AeroLab2_DataSheet_02.csv').to_numpy()
 ProbePosition = np.array(df_1[1:,0], dtype=int)
 PropePressure = np.array(df_1[1:,1:], dtype=float)
 
-BackPressure    = np.array(df_2[0,1:], dtype=int) # psig
-InletTemp       = np.array(df_2[1,1:], dtype=int) # C
-OutletTemp      = np.array(df_2[2,1:], dtype=int) # C
-deltaP          = np.array(df_2[3,1:], dtype=int) # inH2O - Orifice dP
+BackPressure    = np.array(df_2[0,1:], dtype=float) # psig
+InletTemp       = np.array(df_2[1,1:], dtype=float) # C
+OutletTemp      = np.array(df_2[2,1:], dtype=float) # C
+deltaP          = np.array(df_2[3,1:], dtype=float) # inH2O - Orifice dP
 BarometricP     = float(df_2[4,1])                # inHg
 
 # =============================================================================
 # Convert Values
 # =============================================================================
-
+# Convert into SI Units and absolute units (if needed)
 
 
 
 # =============================================================================
 # Make Calculations
 # =============================================================================
-
-
+# Needed Variables
+A_th  = # [m^2] - Throat Area 
+F_a   = # - Thermal Expansion Factor
+C_d   = # - Discharge Coefficient
+Beta  = # d/D - Diameter ratio
+g_c   = 1   # [kg*m/N*s^2] - Gravitational Constant
+# rho_f = # [kg/m^3] - Fluid Density
+# dP   =
 
 
 # =============================================================================
