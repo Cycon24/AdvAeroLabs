24c1b96c1d83fcdd50a2f94949401f36a3a08eab
sladesladeslade <spbrooks4@gmail.com>
2024-09-25 15:32:31 -0400

    added plotting, fixed some vars

--- Lab_02/main.py
+++ Lab_02/main.py
@@ -12,11 +12,11 @@ import pandas as pd
 # =============================================================================
 # Retrieve and organize data
 # =============================================================================
-df_1 = pd.read_csv('AeroLab2_DataSheet.csv').to_numpy()
-df_2 = pd.read_csv('AeroLab2_DataSheet_02.csv').to_numpy()
+df_1 = pd.read_csv(r'Lab_02/AeroLab2_DataSheet.csv').to_numpy()
+df_2 = pd.read_csv(r'Lab_02/AeroLab2_DataSheet_02.csv').to_numpy()
 
 ProbePosition = np.array(df_1[1:,0], dtype=int)
-PropePressure = np.array(df_1[1:,1:], dtype=float)
+ProbePressure = np.array(df_1[1:,1:], dtype=float)
 
 BackPressure    = np.array(df_2[0,1:], dtype=float) # psig
 InletTemp       = np.array(df_2[1,1:], dtype=float) # C
@@ -35,16 +35,39 @@ BarometricP     = float(df_2[4,1])                # inHg
 # Make Calculations
 # =============================================================================
 # Needed Variables
-A_th  = # [m^2] - Throat Area 
-F_a   = # - Thermal Expansion Factor
-C_d   = # - Discharge Coefficient
-Beta  = # d/D - Diameter ratio
-g_c   = 1   # [kg*m/N*s^2] - Gravitational Constant
+A_th  = 0.# [m^2] - Throat Area 
+F_a   = 1.                  # - Thermal Expansion Factor
+C_d   = 0.623               # - Discharge Coefficient
+Beta  = 1.6255/3.1875       # d/D - Diameter ratio
+g_c   = 1.                  # [kg*m/N*s^2] - Gravitational Constant
 # rho_f = # [kg/m^3] - Fluid Density
 # dP   =
+Prats = BarometricP/ProbePressure # TODO: is ProbePressure total? is this eqtn right idfk
 
 
 # =============================================================================
 # # Plot Results
-# =============================================================================
+# =============================================================================\
+# TODO: can use locations or probe positions, unclear which he wants
+locs = np.arange(-0.5, 2.31, 0.1)
+def normPressProfile(Pb, Poss, Prats):
+    plt.figure(f"Normalized Pressure Profile (1a): Pb={Pb:.0f} psig")
+    plt.title(f"Normalized Pressure Profile, Pb={Pb:.0f} psig")
+    plt.plot(Poss, Prats)
+    plt.xlim(-0.5, 2.5); plt.ylim(0, 6)
+    plt.xlabel("Position (in.)"); plt.ylabel(r"$\dfrac{P}{P_0}$")
+    plt.grid()
 
+
+# TODO: check this section, unsure but makes plot
+plt.figure("Normalized Exit Pressure vs Normalized Back Pressure (1b)")
+erats = ProbePressure[25,:]/ProbePressure[0,:]
+brats = BackPressure/ProbePressure[0,:]
+plt.plot(erats, brats)
+plt.grid()
+
+# can generate multiple plots here (comment out if working on dif. section)
+for i in range(8):
+    normPressProfile(BackPressure[i], locs, Prats[:,i])
+
+plt.show()
